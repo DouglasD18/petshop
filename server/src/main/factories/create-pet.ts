cff3074b814c158ba77850b8690265fcf22b010a
Douglinhas <daguiaralcantara@gmail.com>
2023-08-15 14:51:31 -0300

    feat: create makeCreatePetController factorie

---
+++ server/src/main/factories/create-pet.ts
@@ -0,0 +1,13 @@
+import { CreatePetAdapter } from "../../data/useCases/create-pet/create-pet";
+import { CreatePetMongoRepository } from "../../infra/db/mongodb/repositories/create-pet/create-pet";
+import { CreatePetController } from "../../presentation/controllers/create-pet/create-pet";
+import { OwnerValidatorAdapter } from "../../validation/validators/owner-validator/owner-validator";
+import { PetValidatorAdapter } from "../../validation/validators/pet-validator";
+
+export const makeCreatePetController = (): CreatePetController => {
+  const createPetRepository = new CreatePetMongoRepository();
+  const createPet = new CreatePetAdapter(createPetRepository);
+  const ownerValidator = new OwnerValidatorAdapter();
+  const petValidator = new PetValidatorAdapter(ownerValidator);
+  return new CreatePetController(createPet, petValidator);
+}
